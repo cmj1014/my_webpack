759952f417cacef9a39d9bfabb748e710b0a93f1
cmj1014 <cmj1014@163.com>
2021-04-09 10:15:03 +0800

    'src'

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import _ from 'lodash';
+import './style.css';
+import Icon from './icon.jpg';
+
+function component() {
+  let element = document.createElement('div');
+
+  // lodash（目前通过一个 script 引入）对于执行这一行是必需的
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+  var myIcon = new Image(); 
+  myIcon.src = Icon; 
+  element.appendChild(myIcon);
+  return element;
+}
+
+document.body.appendChild(component());
